d395d91d97290144114b03f330933f6d6974ea40
Julmer Puma <julmerpumacondori@gmail.com>
2025-04-02 18:28:48 -0500

    conex eureka

--- ms-pedidos/src/main/java/com/example/mspedidos/config/OpenAPlConfig.java
+++ ms-pedidos/src/main/java/com/example/mspedidos/config/OpenAPlConfig.java
@@ -12,9 +12,9 @@ public class OpenAPlConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI().info(new Info()
-                .title("OPEN API MICROSERVICIO CATÁLOGO")
+                .title("OPEN API MICROSERVICIO PEDIDOS")
                 .version("0.0.1")
-                .description("Servicios web para la gestión del catálogo")
+                .description("Servicios web para la gestión del pedidos")
                 .termsOfService("http://swagger.io/terms")
                 .license(new License().name("Apache 2.0").url("http://springdoc.org"))
         );
